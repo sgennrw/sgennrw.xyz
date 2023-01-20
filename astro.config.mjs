bdcefa859ca94e1987bd27df6a0b072f9bc5000a
sgennrw <nt.salisa@gmail.com>
2023-01-20 23:17:07 +0700

    add navbar and small improvement

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,13 @@ import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
+import partytown from "@astrojs/partytown";
+
+// https://astro.build/config
 export default defineConfig({
-	integrations: [mdx(), tailwind()],
+  integrations: [mdx(), tailwind(), partytown({
+    config: {
+      forward: ["dataLayer.push"],
+    },
+  })]
 });
