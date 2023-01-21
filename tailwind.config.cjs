791ad212ebc969faa2c872f2bad8146f800c8d28
sgennrw <nt.salisa@gmail.com>
2023-01-21 13:13:43 +0700

    🚀 improvement

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,13 @@
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,svg}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,svg}"],
+  theme: {
+    extend: {
+      fontFamily: {
+				inter: ['Inter', 'sans-serif'],
+				lato: ['Lato', 'sans-serif'],
+        mono: ['Roboto Mono', "monospace"],
+      },
+    },
+  },
+  plugins: [],
 };
